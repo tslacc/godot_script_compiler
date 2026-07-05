1c0aa3294f7cc44b694e18051ce2512d4ed30fc4
tslacc <46695088+tslacc@users.noreply.github.com>
2026-07-05 01:14:05 +0000

    Add files via upload

--- main.c
+++ main.c
@@ -1,9 +1,89 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 //Code referenced from https://en.cppreference.com/c/io/fopen
+//No safety for commas inside apostrophes.
+//Specifications: ints and longs are guaranteed (at least) 4 bytes, signed.
+//This program accepts a CSV and outputs a compressed binary file.
+typedef enum opcodes{
+	NOP=0,
+	//Arithmetic instructions
+	ADD=1,	//dest = src1 + src2
+	ADDI=2,	//dest = src1 + imm
+	SUB=3,	//see add
+	SUBI=4,
+	MUL=5,	//dest = src1 * src2
+	MULI=6,	//dest = src1 * imm
+	DIV=7,	//see mul
+	DIVI=8,
+	//All jump instructions are absolute jumps by instruction position
+	//All jump arguments shall be registers
+	//Jump to arg0 if...
+	JMP=9,	//true
+	JE=10,	//arg1 == arg2
+	JNE=11,	//arg1 != arg2
+	JL=12,	//arg1 < arg2
+	JG=13,	//arg1 > arg2
+	JMPR=14,	//JMP to arg0's value
+	//Write/read functions reference non-register memory by string reference
+	//Implementation of what each string refers to is left to the program caller
+	//Always refers to LOCATION first, then the register to read/write from.
+	WRITE=15,	
+	READ=16,	
+	FUNC=17	//func(args...) where args is a single string.
+} OPC;
+//Reads at most count-1 characters from stream and writes them to str, IFF there is a comma or newline in that sequence.
+//Returns str on success and NULL on failure.
+//Exit program on fail to read properly.
+char *fgets_comma(char *str, int count, FILE *stream){
+	char *work = str;
+	int c = 0;
+	while ((c = fgetc(stream)) != EOF && count > 0) {
+		if (c == ',' || c == '\n') {
+			*work = '\0';
+			return str;
+		}
+		*work = c;
+		work++;
+		count--;
+	}
+	if (c == EOF) {
+		printf("End of file\n");
+	} else {
+		printf("Error when reading file, last char is %c %u\n",c,c);
+	}
+	return NULL;
+}
+char *fgets_newline_only(char *str, int count, FILE *stream){
+	char *work = str;
+	int c = 0;
+	while ((c = fgetc(stream)) != EOF && count > 0) {
+		if (c == '\n') {
+			*work = '\0';
+			return str;
+		}
+		*work = c;
+		work++;
+		count--;
+	}
+	if (c == EOF) {
+		printf("End of file\n");
+	} else {
+		printf("Error when reading file, last char is %c %u\n",c,c);
+	}
+	return NULL;
+}
+//Read up to 32 bytes and return the next int delimited by a comma or newline.
+int next_int(FILE *stream){
+	const int READ_SIZE = 32;
+	char *ptr = malloc(READ_SIZE*sizeof(int));
+	ptr = fgets_comma(ptr, READ_SIZE, stream);
+	int result = atoi(ptr);
+	free(ptr);
+	return result;
+}
 int main(int argc, char * argv[])
 {
-
 	if (argc > 1) {
 		printf("Rcvd value %s\n", argv[1]);
 	} else {
@@ -27,10 +107,10 @@ int main(int argc, char * argv[])
     }
 
     int c; // note: int, not char, required to handle EOF
+
     while ((c = fgetc(fp)) != EOF) {// standard C I/O file reading loop
         if(c == '\n') printf("\\n");
         putchar(c);
-
 	}
     if (ferror(fp))
         puts("I/O error when reading");
@@ -39,7 +119,60 @@ int main(int argc, char * argv[])
         puts("End of file is reached successfully");
         is_ok = EXIT_SUCCESS;
     }
-
+    //Begin custom code
+	rewind(fp);
+	is_ok = EXIT_FAILURE;
+	if(!fp){
+		perror("File opening failed");
+		return is_ok;
+	}
+	printf("Begin custom read\n");
+	while (!feof(fp)) {// standard C I/O file reading loop
+ 		//Initial state. 
+ 		//String holder
+ 		char *str = malloc(512*sizeof(char));
+ 		str = fgets_comma(str, 8, fp);
+ 		if (str == NULL) {
+ 			if (!feof(fp)) {
+	 			perror("Failed to read operator. Closing file.\n");
+	 			fclose(fp);
+	 			return is_ok;
+	 		}
+	 		break;
+ 		}
+ 		//Write str to file as an integer here.
+		printf("Instruction %s ", str);
+		//If this is any of the following instructions, get two integers and write to file.
+ 		if (strcmp(str, "ADD") == 0 || strcmp(str, "ADDI") == 0 || strcmp(str, "SUB") == 0 || strcmp(str, "SUBI") == 0 || strcmp(str, "MUL") == 0 || strcmp(str, "MULI") == 0 || strcmp(str, "DIV") == 0 || strcmp(str, "DIVI") == 0 || strcmp(str, "JE") == 0 || strcmp(str, "JNE") == 0 || strcmp(str, "JE") == 0 || strcmp(str, "JL") == 0 || strcmp(str, "JG") == 0)
+		{
+			int n = next_int(fp);
+			printf("%d ",n);
+			n = next_int(fp);
+			printf("%d\n",n);
+		} else if (strcmp(str, "JMP") == 0 || strcmp(str, "JMPR") == 0){
+			int n = next_int(fp);
+			printf("%d\n",n);
+		} else if (strcmp(str, "WRITE") == 0 || strcmp(str, "READ") == 0){
+			str = fgets_comma(str,512,fp);
+			if (str == NULL){
+				perror("Failed to read string operand.\n");
+				fclose(fp);
+				return is_ok;
+			}
+			printf("%s ",str);
+			int n = next_int(fp);
+			printf("%d\n",n);
+		} else if (strcmp(str, "FUNC") == 0) {
+			str = fgets_newline_only(str,512,fp);
+			if (str == NULL){
+				perror("Failed to read string operand.\n");
+				fclose(fp);
+				return is_ok;
+			}
+			printf("%s\n",str);
+		}
+	}
     fclose(fp);
     return is_ok;
 }
+//TODO Past line 146 change all prints to file writes.
